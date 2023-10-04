90e43f8e8eae446f4dd050efaa39426027132905
Milan Mishra <raj.famous007+1@gmail.com>
2023-10-04 10:32:28 +0530

    Mongoose model fix and connection changes

--- src/model/index.js
+++ src/model/index.js
@@ -1,12 +1,17 @@
 module.exports = () => {
-    const mongoose = require('mongoose');
+  const mongoose = require("mongoose");
 
-    //to connect
-    mongoose.connect(process.env.MONGOOES_PATH, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
-    }, err => err
+  // Connect to the mongoose.
+  mongoose.connect(
+    process.env.MONGOOES_PATH,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    },
+    (err) =>
+      err
         ? console.log("Enable to connect to the database due to:", err)
-        : console.log("MongoDB connection sucessfull!"));
-}
+        : console.log("MongoDB connection successful!")
+  );
+};
--- src/model/teacher_model.js
+++ src/model/teacher_model.js
@@ -1,16 +1,19 @@
 module.exports = () => {
-    const mongoose = require('mongoose');
+  const mongoose = require("mongoose");
 
-    const TeacherSchema = new mongoose.Schema({
-        name: String,
-        email: String,
-        students: {
-            type: [mongoose.ObjectId],
-            ref: 'student_data'
-        }
-    }, {
-        versionKey: false
-    });
+  const TeacherSchema = new mongoose.Schema(
+    {
+      name: String,
+      email: String,
+      students: {
+        type: [mongoose.ObjectId],
+        ref: "student_data",
+      },
+    },
+    {
+      versionKey: false,
+    }
+  );
 
-    return mongoose.model('teacher_data', TeacherSchema);
+  return mongoose.model("teacher_data", TeacherSchema);
 };
